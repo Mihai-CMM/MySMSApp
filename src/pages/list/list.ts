2cef8bc8a9e46af03c2071bc12ce4f4be5625ad1
Costache Mircea Mihai <costache.mircea.mihai@gmail.com>
2017-05-03 21:50:18 +0300

    Revert to ionic datepicker

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -9,62 +9,30 @@ import { DatePicker } from '@ionic-native/date-picker';
   providers: [Calendar, DatePicker]
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  names: string[];
-  phones: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  startDate: any;
+  endDate: any;
 
   appoiment = {
    name: "",
    number: "",
-   info:""
+   info:"",
+   //startDate: new Date(),
+   //endDate: new Date()
   };
-
 
 
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public calendar: Calendar, private datePicker: DatePicker ) {
 
-
     this.calendar.createCalendar('NoiProgramari').then(
      (msg) => { console.log(msg); },
      (err) => { console.log(err); }
    );
-
-
   }
 
 
 
-  // CODE FOR LOCAL CALENDAR _ IT CAB ONLY CREATES CALENDAR BUT IT CANT ADD EVENTS ON S7
-
-
-   startDate(){
-      this.datePicker.show({
-        date: new Date(),
-        mode: 'datetime',
-        androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK,
-        allowOldDates: false
-      }).then(
-      date => console.log('Got date: ', date),
-      err => console.log('Error occurred while getting date: ', err)
-    );
-  }
-
-
-   endDate(){
-      this.datePicker.show({
-        date: new Date(),
-        mode: 'datetime',
-        androidTheme: this.datePicker.ANDROID_THEMES.THEME_HOLO_DARK,
-        allowOldDates: false
-      }).then(
-      date => console.log('Got date: ', date),
-      err => console.log('Error occurred while getting date: ', err)
-    );
-  }
 
 
 
@@ -72,10 +40,11 @@ export class ListPage {
    addAppoiment(){
 
     this.calendar.requestWritePermission()
-    //this.calendar.createEvent(this.appoiment.name, this.appoiment.number, this.appoiment.info, this.appoiment.startDate, this.appoiment.endDate);
-    //alert('Data de inceput: ' + this.appoiment.startDate);
-    //alert('Data de sfarsit: ' + this.appoiment.endDate);
+    this.calendar.createEvent(this.appoiment.name, this.appoiment.number, this.appoiment.info, this.startDate, this.endDate);
+    console.log('Data de inceput: ' , this.startDate);
+    console.log('Data de sfarsit: ' , this.endDate);
    };
+
 
 
   itemTapped(event, item) {
